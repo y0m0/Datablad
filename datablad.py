fee1a0db20442cf14bedba6ecf182ae8aaa66d14
y0m0 <lixi.daniele@gmail.com>
2016-04-20 15:27:42 +0200

    change datablad.py to reflect template.html

--- datablad.py
+++ datablad.py
@@ -25,41 +25,95 @@ def make_html(prod_dict):
 
         doc.asis('<!DOCTYPE html>')
         with tag('html', lang='eng'):
+
             with tag('head'):
                 doc.asis('<meta charset="UTF-8" />')
                 doc.asis('<link rel="stylesheet" type="text/css" href="style.css">')
                 doc.asis('<meta name="viewport" content="width=device-width">')
                 with tag('title'):
                     text(prod.prodid.text)
+
             with tag('body', klass='main'):
-                with tag('p', klass='prodid'):
-                    text('prodid: ' + prod.prodid.text)
-                with tag('h4', klass='title'):
-                    text('Produktnavn: ')
-                with tag('h1', klass='prodname'):
-                    text(prod.desc.text)
-                with tag('h2', klass='ingredienser'):
-                    text('Ingredienser')
-                with tag('p', klass='ingredienser'):
-                    if hasattr(prod, 'prodnote'):
-                        text(prod.prodnote.text)
-                    else:
-                        text('no data yet')
-                with tag('h2', klass='energy'):
-                    text('Næringsinneholder for 100g')
-                with tag('p', klass='energy'):
-                    if hasattr(prod, 'technote'):
-                        text(prod.technote.text)
-                    else:
-                        text('no data yet')
-                with tag('h2', klass='allergener'):
-                    text('Allergener')
-                with tag('p', klass='allergener'):
-                    text('allergener....')
-                with tag('h2', klass='shelf'):
-                    text('Oppbevaring')
-                with tag('p', klass='shelf'):
-                    text('shelf-life, temp')
+                with tag('header', klass='main-header'):
+                    doc.stag('img', klass='logo', src='logo.jpg', alt='Villa Import AS')
+                    with tag('h1', klass='prodname'):
+                        text(prod.desc.text)
+
+
+                with tag('div', klass='content'):
+                    with tag('div', klass='primary-content group'):
+
+                        with tag ('div', klass='product-picture'):
+                            doc.stag('img', src='102029.jpg', alt='Product Picture')
+
+
+                        with tag('div', klass='product-details'):
+
+                            with tag('div', klass='basic-info'):
+                                with tag('h2'):
+                                    text('Produktinformasjon')
+                                with tag('ul', klass='field'):
+                                    with tag('li'):
+                                        text('Varenummer: ' + prod.prodid.text)
+                                    with tag('li'):
+                                        text('Produktgruppe: ' + prod.prodgroupex.text)
+                                    with tag('li'):
+                                        text('Pakning: ' + prod.weight.text + prod.weightunit.text)
+
+                            with tag('div', klass='ingredients'):
+                                with tag('h2'):
+                                    text('Ingredienser')
+                                with tag('p', klass='field'):
+                                    # if hasattr(prod, 'prodnote'):
+                                    #     text(prod.prodnote.text)
+                                    # else:
+                                    #     text('no data yet')
+                                    text(prod.prodnote.text)
+
+
+                    with tag('div', klass='secondary-content'):
+
+                        with tag('div', klass='energy'):
+                            with tag('h2'):
+                                text('Næringsinnehold Pr. 100 gram.')
+                            with tag('ul', klass='field'):
+                                with tag('li'):
+                                    text('Energi(kcal/kj) 437/1931')
+                                with tag('li'):
+                                    text('Fett 45g')
+                                with tag('li'):
+                                    text('hvorav Mettet Fett 5,4g')
+                                with tag('li'):
+                                    text('Karbohydrat 4,2g')
+                                with tag('li'):
+                                    text('hvorav Sukker 1,5g')
+                                with tag('li'):
+                                    text('Protein 4,4g')
+                                with tag('li'):
+                                    text('Salt 2,2g')
+                                with tag('li'):
+                                    text('Kostfiber 0,7g')
+
+                        with tag('div', klass='allergens'):
+                            with tag('h2'):
+                                text('Allergener')
+                            with tag('ul', klass='field'):
+                                for i in range(4):
+                                    with tag('li'):
+                                        text('....')
+
+                        with tag('div', klass='storage'):
+                            with tag('h2'):
+                                text('Oppbevaring')
+                            with tag('ul', klass='field'):
+                                with tag('li'):
+                                    text('....')
+
+                with tag('footer', klass='main-footer'):
+                    with tag('p'):
+                        doc.asis('<a href="www.villaimport.no">www.villaimport.no</a> &#9702; <a href="tel:+4723229999">+47 23229999</a> &#9702; <a href="mailto:mail@villaimport.no">mail@villaimport.no</a>')
+                    with tag('p'):
+                        doc.asis('&copy; Villa Import AS - 2016')
 
         with codecs.open(prodcode + '.html', 'w', 'utf-8') as file:
             file.write(indent(doc.getvalue()))
